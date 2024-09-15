ab1dca704a9f5ef537e7f2d71359f2f36319b9ea
Patty <125532741+Mabvuu@users.noreply.github.com>
2024-09-15 19:30:02 +0200

    Add files via upload

--- contracts/chai.sol
+++ contracts/chai.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
 contract chai{
@@ -11,18 +10,18 @@ contract chai{
         address from;
     }
 
-
     Memo[] memos;
     address payable owner; //owner is going to receive funds
     constructor(){
         owner = payable(msg.sender);
+    }
 
-    }
-    function buyChai(string calldata name, string calldata message) external payable{
-        require(msg.value>0,"Please Pay more than 0");
+    function buyChai(string calldata name,string calldata message) external payable{
+        require(msg.value>0,"Please pay more than 0 ether");
         owner.transfer(msg.value);
-        memos.push(Memo(name,message, block.timestamp,msg.sender));
+        memos.push(Memo(name,message,block.timestamp,msg.sender));
     }
+
     function getMemos() public view returns(Memo[] memory){
         return memos;
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,19 @@
+
 const hre = require("hardhat");
 
 async function main() {
-  // Get the chai contract to deploy
-  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
   const chai = await Chai.deploy(); //creating an instance of our smart contract
 
-  // Wait for chai deployment to finish
-  await chai.deployed();
-  console.log("Chai contract deployed to:", chai.address);
+  await chai.deployed();//deploying your smart contract
 
-  // If you want to deploy Lock too, get the Lock contract to deploy
-  const Lock = await hre.ethers.getContractFactory("Lock");
-
-  // Set the required parameters for Lock, e.g., unlockTime and lockedAmount
-  const unlockTime = Math.floor(Date.now() / 1000) + 60; // Unlock time 1 minute from now
-  const lockedAmount = hre.ethers.utils.parseEther("1"); // Lock 1 Ether
-
-  // Deploy the Lock contract with parameters
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  // Wait for Lock deployment to finish
-  await lock.deployed();
-  console.log("Lock contract deployed to:", lock.address);
+  console.log("Deployed contract address:",`${chai.address}`);
 }
 
-// Call the main function and handle any errors
-main()
-  .then(() => process.exit(0)) // Exit the process if successful
-  .catch((error) => {
-    console.error(error); // Log any errors
-    process.exit(1); // Exit the process with an error code
-  });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+//0xa64e3144835aF8781c750ceC432784a68d883266
